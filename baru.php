e7430b0b64d269877b672971d11f5a0d3a496fc9
wiradian84 <wiradian84@gmail.com>
2018-06-18 21:37:59 +0700

    update

--- baru.php
+++ baru.php
@@ -1,28 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ini halaman percobaan</title>
+	<title>Judul Halaman</title>
 </head>
 <body>
-	<h1>Ini judul</h1>
-	<?php 
-		$angka = 2018;
-	 ?>
+	<h1>Judul Paragraf</h1>
+<?php 
+	$angka = 3;
+	$angka = "enam";
+	$subjudul = "Ini adalah subjudul";
+	var_dump($angka);
+	$logika = true;
+	$logika_dua = false;
+
+	$hasil = var_dump($logika || $logika_dua);
+	echo $hasil;
+
+	if ($logika&&$logika_dua) {
+		echo "ini bisa jalan";
+	} else {
+		echo "ini tidak jalan";
+	}
+
+	$perasaan = "kayaknya sih";
+?>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<?php 
-		echo "<h2>Ini tulisan ".$angka."</h2>";
-	 ?>
-	 <?php echo "Ini tulisan baru"; ?>
-	 <?= "Ini tulisan baru" ?>
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+	<h2><?= $subjudul ?></h2>
+	<p>
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<?php echo $angka ?>
+	</p>
+	
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php 
+//ini merupakan komentar
+// walau sepanjang apapun
+	echo "Ini adalah tester<br>";
+	echo "Nama saya dian<br>";
+// komentar yang tertulis di sini
+// ia tak kan pernah kuanggap ada
+$angka = "satu";
+$lagi = "satu";
+$kata = 2;
 
-
-echo "Ini adalah tester";
-
+echo $angka.$kata.$lagi."<br>";
+echo "Berat badan anak saya adalah ".$kata." kg";
 
  ?>
